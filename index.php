845a271029b3f61a63f337b2a2c6107ee26ce380
M King <matthew.king@channelgrabber.com>
2014-11-06 11:12:41 +0000

    CGIV-3925 move to using config and pruning

--- index.php
+++ index.php
@@ -42,8 +42,8 @@ if (!empty($errors)) {
 $command = sprintf('%s build %s %s', $config['bin'], $config['json'], $config['webroot']);
 if (isset($_GET['package'])) {
     $command .= ' ' . $_GET['package'];
-    chdir('/tmp/satis/repositories/' . $_GET['package']);
-    exec('git fetch origin');
+    chdir($config['repositories'] .  '/' . $_GET['package']);
+    exec('git fetch origin && git remote update --prune origin');
     chdir(__DIR__);
 }
 if (null !== $config['user']) {
